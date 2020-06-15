bf0fbcf43ea781d4dbd51c34bd6d50241d988819
Kashif <kashifshahzad@yours.com>
2020-06-15 17:54:00 +0300

    Java Script Variables

--- story.js
+++ story.js
@@ -1,7 +1,11 @@
 // Define the name here on line 2
-
+let Name = "Kashif";
 // Define the age here on line 4
-
+let Age = 32;
 // Define the hobby here on line 6
-
+let Hobby = "Cricket";
 // Print the story here on line 8
+let Story = `Mr. ${Name}, in 33 years, you will be ${
+  Age + 33
+} years old. By then you'll have decades of ${Hobby} practice!`;
+console.log(Story);
